b2bf01520988c5a4733e5314516d91c5a086239e
Rahaf Shora <rahaf.it@gmail.com>
2023-04-02 13:20:18 +0100

    Database Structure

---
+++ database/migrations/2023_04_02_114642_create_components_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('components', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->string('serial_number');
+            $table->foreignId('turbine_id')->constrained();
+            $table->foreignId('component_type_id')->constrained();
+            $table->unique(['component_type_id', 'serial_number']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('components');
+    }
+};
